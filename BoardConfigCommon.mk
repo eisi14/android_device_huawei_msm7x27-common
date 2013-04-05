b7336d6a0071813e7e1ddc8491d6acacbeefe794
Eisi14 <eismann14@googlemail.com>
2013-04-05 13:37:40 +0200

    Initial commit of common msm7x27 files.

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,136 @@
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+## Camera
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
+BOARD_USES_LEGACY_CAMERA := true
+BOARD_CPU_COLOR_CONVERT := true
+
+## Kernel
+TARGET_KERNEL_SOURCE := kernel/huawei/msm7x27
+# Copy LG Kernel Headers here if necessary, DON'T use Adroid auto-generated headers
+TARGET_SPECIFIC_HEADER_PATH := device/huawei/msm7x27-common/include
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=qcom no_console_suspend
+
+## CPU & Platform
+ARCH_ARM_HAVE_VFP := true
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_BOARD_PLATFORM := msm7x27
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+
+## Browser & WebKit
+JS_ENGINE := v8
+HTTP := chrome
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+## Boot loader & recovery
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+## Partition Sizes
+# Fix this up by examining /proc/mtd on a running device
+BOARD_KERNEL_BASE := 0x12800000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x004400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x005000000
+# Ignore that this is much larger than the 190Mb allowed.
+# It's only for resolving build issues with the system.img
+# being too big. It will still be only about 100MB MAXIMUM
+# because all the useless trash is now in extras
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xE600000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c780000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+## Graphics
+# Enable OpenGL Hardware Acceleration
+USE_OPENGL_RENDERER := true
+# Force non usage of VSync
+TARGET_NO_HW_VSYNC := true
+TARGET_USES_GENLOCK := true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_USE_SKIA_LCDTEXT := true
+BOARD_EGL_CFG := device/huawei/msm7x27-common/configs/egl.cfg
+# Framerate
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+
+## Qualcomm and Adreno Hardware
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+COMMON_GLOBAL_CFLAGS += -DMISSING_GRALLOC_BUFFERS
+# Add support for OMX
+COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
+
+## GPS
+# Enable the GPS HAL & AMSS version to use for GPS
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+#BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+## Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+## Audio, Bluetooth & FM Radio
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_AUDIO_LEGACY := false
+BOARD_COMBO_DEVICE_SUPPORTED := true
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_FM_DEVICE := bcm4325
+BOARD_HAVE_FM_RADIO := true
+# Use the second one (QCOM_FM_ENABLED) not the first
+# The first breaks the builds and the second is more
+# recent and is used in the CM code
+# COMMON_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+# COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED
+
+## RIL
+BOARD_PROVIDES_LIBRIL := true
+
+## Mass Storage for Jelly Bean
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
+
+## Touch screen compatibility for Jelly Bean
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+## Wi-Fi & Wi-Fi HotSpot
+BOARD_WPA_SUPPLICANT_DRIVER     := WEXT
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WLAN_DEVICE                := bcm4329
+WIFI_BAND                        := 802_11_ABG
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/firmware.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/firmware_apsta.bin"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/firmware.bin nvram_path=/system/etc/wifi/nvram.txt"
+WIFI_DRIVER_MODULE_NAME          := "dhd"
+
+
+## Command line for charging mode
+BOARD_CHARGING_CMDLINE_NAME := "lge.reboot"
+BOARD_CHARGING_CMDLINE_VALUE := "pwroff"
+BOARD_USES_RECOVERY_CHARGEMODE := false
+
+## OTA script
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/huawei/msm7x27-common/releasetools/ota_from_target_files
---
+++ device.mk
@@ -0,0 +1,157 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/extras.sh:system/bin/extras.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/initlogo.rle:root/initlogo.rle \
+    $(LOCAL_PATH)/root/init.qcom.rc:root/init.qcom.rc \
+    $(LOCAL_PATH)/root/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(LOCAL_PATH)/root/init.qcom.sh:root/init.qcom.sh \
+    $(LOCAL_PATH)/root/ueventd.qcom.rc:root/ueventd.qcom.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/CHANGELOG-CM.txt:system/etc/CHANGELOG-CM.txt \
+    $(LOCAL_PATH)/configs/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    $(LOCAL_PATH)/configs/adreno_config.txt:system/etc/adreno_config.txt \
+    $(LOCAL_PATH)/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
+    $(LOCAL_PATH)/configs/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/nvram.txt:system/etc/wifi/nvram.txt \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.camera=msm7x27 \
+    debug.camcorder.disablemeta=1 \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.3_r6 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    ro.telephony.call_ring.multiple=false \
+    ro.vold.umsdirtyratio=20
+
+# Battery life hacks
+PRODUCT_PROPERTY_OVERRIDES += \
+    # Turn off RIL when not needed
+    ro.ril.disable.power.collapse=1
+    # Better sleep system
+    pm.sleep_mode=1
+    # Scan for WiFi less often to increase batery life
+    wifi.supplicant_scan_interval=180
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true \
+    com.qc.hdmi_out=false \
+    debug.sf.hw=1 \
+    debug.enabletr=false \
+    debug.hwui.render_dirty_regions=false \
+    debug.composition.type=mdp \
+    debug.gr.numframebuffers=2 \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    hwui.print_config=choice \
+    persist.sys.strictmode.visual=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-meta=false \
+    media.stagefright.enable-scan=false \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio_policy.msm7x27 \
+    audio.primary.msm7x27 \
+    audio.a2dp.default \
+    libaudioutils
+
+# Display
+PRODUCT_PACKAGES += \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer \
+    libopencorehw \
+    gralloc.msm7x27 \
+    copybit.msm7x27 \
+    hwcomposer.msm7x27
+
+# Media
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libmm-omxcore \
+    libOmxCore \
+    libdivxdrmdecrypt
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    gps.default \
+    lights.msm7x27 \
+    lgapversion
+    
+# Build sim toolkit
+PRODUCT_PACKAGES += \
+    Stk
+    
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm7x27 \
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# BT startup
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+PRODUCT_PACKAGES += \
+    hcitool \
+    hciconfig \
+    hwaddrs
+
+# Offmode charging
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/msm7x27-common/overlay
